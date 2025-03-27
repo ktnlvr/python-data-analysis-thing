f819a1e7b630f0a8c0733ef31d3fd79ba0914039
ktnlvr <artur.roos.artur@gmail.com>
2025-03-27 14:17:15 +0200

    Prefer pure python files

---
+++ artur.py
@@ -0,0 +1,16 @@
+# %%
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import kagglehub
+from os.path import join as path_join
+
+# %%
+data_root = kagglehub.dataset_download("andrewkronser/cve-common-vulnerabilities-and-exposures")
+
+# %%
+df = pd.read_csv(path_join(data_root, 'cve.csv'), header=0, index_col=0)
+df.mod_date = pd.to_datetime(df.mod_date)
+df.pub_date = pd.to_datetime(df.pub_date)
+
+df.info() 
