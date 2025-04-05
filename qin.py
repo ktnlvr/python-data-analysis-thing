352690cb9b37229bb6b0a2425c12408f112e1ff4
01Qin <sisukasqin@hotmail.com>
2025-04-05 17:46:12 +0300

    Calculate the geometric mean of cvss, cwe_code

--- qin.py
+++ qin.py
@@ -9,6 +9,19 @@ from os.path import join as path_join
 data_root = kagglehub.dataset_download("andrewkronser/cve-common-vulnerabilities-and-exposures")
 
 # %%
+## 📌 1. Loading the Dataset
+
+# >## 💡 **Interpretation**:
+# -   **mod_date: The date the entry was last modified.**
+# -  **pub_date: The date the entry was published.**
+# -  **cvss: Common Vulnerability Scoring System (CVSS) score, a measure of the severity of a vulnerability.**
+# -  **cwe_code: Common Weakness Enumeration (CWE) code, identifying the type of weakness.**
+# -  **cwe_name: The name associated with the CWE code.**
+# -  **summary: A text summary of the vulnerability.**
+# -  **access_authentication.**
+# -  **access_complexity: how difficult it is to execute.**
+# -  **access_vector: how the attack is performed, aka via network or locally.**
+
 df = pd.read_csv(path_join(data_root, 'cve.csv'), header=0, index_col=0)
 df.mod_date = pd.to_datetime(df.mod_date)
 df.pub_date = pd.to_datetime(df.pub_date)
